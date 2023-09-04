08315b2d33cd2c2a95c37d36814e188f71787e88
mazhariqbasiddiquee <mazhariqbal963169u@gamil.com>
2023-09-04 11:36:14 +0530

    KL

--- Backend/index.js
+++ Backend/index.js
@@ -11,11 +11,7 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    // Other CORS headers and middleware settings
-    next();
-  });
+
 app.use("/user",userRouter)
 app.use("/question",questionRouter)
 
